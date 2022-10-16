ba03b0e45f749697a4d107cc608eb5ae71c28f8e
Boris Villanueva <borisvillanueva@yahoo.com>
2022-10-16 07:19:59 -0600

    initial

---
+++ js/app.js
@@ -0,0 +1,5 @@
+//Variables
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBTN = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
